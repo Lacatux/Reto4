36f49441fa95a03ac894400514d7c84243bc05b8
MonsBaan <ibai3553@gmail.com>
2021-02-08 14:17:03 +0100

    JavaScript Menu

--- WEB/js/scrollDown.js
+++ WEB/js/scrollDown.js
@@ -4,19 +4,16 @@ function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("menu").style.padding = "5px 10px";
     document.getElementById("logo").style.fontSize = "25px";
-
     var x = document.querySelectorAll('#nav');
     for (var i = 0; i < x.length; i++) {
         var currentEl = x[i];
         currentEl.style.padding = '5px 5px';
     }
-
     var x = document.querySelectorAll('#login');
     for (var i = 0; i < x.length; i++) {
         var currentEl = x[i];
         currentEl.style.padding = '5px 5px';
     }
-
   } else {
     document.getElementById("menu").style.padding = "30px 10px";
     document.getElementById("logo").style.fontSize = "35px";
@@ -32,4 +29,5 @@ function scrollFunction() {
     }
 
   }
+
 }
