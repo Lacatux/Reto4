296e4b15a93ba8c0665af54bc6f494b269f19b4a
MonsBaan <ibai3553@gmail.com>
2021-02-17 10:23:47 +0100

    AUE EU AEAE

--- WEB/index.php
+++ WEB/index.php
@@ -8,7 +8,9 @@
 
     <script src="js/funcionesJs.js">
     </script>
-
+    <?php
+    include("php/datos.php");
+     ?>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
   </head>
--- WEB/php/datos.php
+++ WEB/php/datos.php
@@ -1,19 +1,32 @@
 <?php
 function conectarBD(){
-  //$MYDB = "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.6.172)(PORT = 1539))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = XE)))";
-  $connect = oci_connect("logingenerico", "Nombre_Generico2020", "192.168.6.172:1539/xe");
-  if ($connect) {
-    echo "Conectado";
-  }else {
-    echo "Error de conexion";
-  }
-oci_close($connect);
+  $db = "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL =TCP)
+  (HOST = 192.168.6.172)(PORT = 1539)))(CONNECT_DATA=(SID=XE)))";
 
-  /*$mysqli = new mysqli ("127.0.0.1", "phpRoot", "123", "mastodon");
-  if ($mysqli->connect_errno) {
-    echo "Fallo en la conexion: ".$mysqli->connect_errno;
-  }*/
-  return $connect;
+  $conn = oci_connect('basegenerica', 'Nombre_Generico2020', $db);
+  if (!$conn) {
+      $e = oci_error();
+      trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+  }
+  return $conn;
+
+  oci_close($conn);
 }
 //====================================================================
+function pruebaSel(){
+  $conn = conectarBD();
+  $stid = oci_parse($conn, "SELECT * FROM profesor");
+  oci_execute($stid);
+
+  echo "<table border='1'>\n";
+  while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+    echo "<tr>\n";
+    foreach ($row as $item) {
+        echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "") . "</td>\n";
+    }
+    echo "</tr>\n";
+}
+echo "</table>\n";
+
+}
 ?>
